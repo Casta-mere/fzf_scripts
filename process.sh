276127efc6e6c8fdaaf7826101685a1ddb39be69
Casta-mere <castamerego@gmail.com>
2026-06-03 16:45:20 +0800

    feat: add ignore dir for ffgrep ff

--- process.sh
+++ process.sh
@@ -6,27 +6,40 @@ fkill() {
   fi 
 }
 
+_ff_ignore_dirs=(node_modules .git __pycache__ .venv venv dist build .next target .cache)
+
 ffgrep() {
   local query="$*"
   local ans
-  local cmd_height=$(awk "BEGIN { printf \"%d\", $(tput lines) * 0.8 - 6 }") 
+  local cmd_height=$(awk "BEGIN { printf \"%d\", $(tput lines) * 0.8 - 6 }")
   local offset=$(awk "BEGIN { printf \"%d\", $cmd_height * 0.5 }")
+  local exclude_args=()
+  for d in "${_ff_ignore_dirs[@]}"; do
+    exclude_args+=(--exclude-dir="$d")
+  done
 
-  ans=$(grep -rnI --color=always -E "$query" . 2>/dev/null | \
+  ans=$(grep -rnI --color=always -E "${exclude_args[@]}" "$query" . 2>/dev/null | \
     fzf --ansi \
         --delimiter ':' \
         --height=80% --reverse \
         --preview='batcat --color=always --paging=never {1} --highlight-line={2} --wrap=character' \
         --preview-window=right:60%,wrap,+{2}-$offset \
+        --bind 'ctrl-p:toggle-preview' \
     )
-    
+
     if [[ -n "$ans" ]]; then
       echo $ans | head -n1 | awk -F: '{print $1":"$2}'
     fi
 }
 
 ff() {
-  find . -type f 2>/dev/null |
+  local prune_args=()
+  for d in "${_ff_ignore_dirs[@]}"; do
+    prune_args+=(-not -path "*/$d/*")
+  done
+  find . -type f "${prune_args[@]}" 2>/dev/null |
     fzf --preview 'batcat --color=always --style=header,grid --line-range :100 {}' \
-        --preview-window=right:60%:wrap
+        --preview-window=right:60%:wrap \
+        --bind 'ctrl-p:toggle-preview' \
+
 }
